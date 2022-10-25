18ed11e488e098ead0b2eb939f0ea5b5d5be81d9
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-10-26 03:02:21 +0530

    stripe integration compelete

--- public/script.js
+++ public/script.js
@@ -1,4 +1,23 @@
 const checkoutCTA = document.querySelector(".cta-checkout");
 checkoutCTA.addEventListener("click", () => {
-  console.log("Im the checkout button");
+  fetch("http://localhost:4000/create-checkout-session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      items: [
+        { id: 1, quantity: 3 },
+        { id: 2, quantity: 2 },
+      ],
+    }),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+      return res.json().then((json) => Promise.reject(json));
+    })
+    .then(({ url }) => {
+      window.location = url;
+    })
+    .catch((e) => console.log(e));
 });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const userItems = require("../userItems/serverItems.json");
+const storeItems = new Map([
+  [1, { priceInCents: 400, name: "Stock Market 101" }],
+  [2, { priceInCents: 500, name: "A turd in the wind" }],
+]);
+
+const stripe = require("stripe")(
+  "sk_test_51Lw6hSSBtSNzCDMOMt89yR7DrfKSJ5uMHQg9U25gA5ndvwvSJwEQDEkidLb6M7PYvu975bE7J58uQL523CIJgbgf00rkI5xiFh"
+);
 
 const app = express();
 const port = 4000;
@@ -18,8 +26,31 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create-checkout-session", async (req, res) => {
-  res.json({ url: "DaddyYay" });
+  const purchaseItems = req.body.items;
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: purchaseItems.map((item) => {
+        const storeItem = storeItems.get(item.id);
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInCents,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `http://localhost:4000/success.html`,
+      cancel_url: `http://localhost:4000/cancel.html`,
+    });
+    res.json(session);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.listen(port, () => console.log(`server fired up on port:${port}`));
